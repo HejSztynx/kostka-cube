fed5323787469be59ab2b88375c04021358120d0
HejSztynx <shaggyic1@onet.eu>
2025-07-15 16:07:32 +0200

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,6 @@ const Z_INIT: f32 = 5.0;
 const ZP: f32 = 5.0;
 const SOMETHING: f32 = 10.0;
 
-
-// const SQRT_3: f32 = 1.73205;
-const SQRT_2: f32 = 1.41421;
-
 const SCREEN_X: usize = 70;
 const SCREEN_Y: usize = 45;
 
@@ -110,10 +106,6 @@ impl Cubie {
 
     fn faces(&self) -> Vec<Face> {
         let h = CUBE_SIZE / 2.0;
-        // let r = half * SQRT_2;
-        // let (x, y, z) = (self.position.x, self.position.y, -r);
-        // let y_top = y + half;
-        // let y_bot = y - half;
 
         let initial_corners = [
             Position { x: h, y: h, z: -h },
@@ -131,18 +123,6 @@ impl Cubie {
             .into_iter()
             .map(|p| p.rotate_y(self.rotation_y).rotate_x(self.rotation_x).translate(self.position))
             .collect();
-
-        // let corners = [
-        //     Position{x: x * rot_y_cos + z * rot_y_sin, y: y_top, z: self.position.z + -x * rot_y_sin + z * rot_y_cos},
-        //     Position{x: x * -rot_y_sin + z * rot_y_cos, y: y_top, z: self.position.z + -x * rot_y_cos + z * -rot_y_sin},
-        //     Position{x: x * -rot_y_cos + z * -rot_y_sin, y: y_top, z: self.position.z + -x * -rot_y_sin + z * -rot_y_cos},
-        //     Position{x: x * rot_y_sin + z * -rot_y_cos, y: y_top, z: self.position.z + -x * -rot_y_cos + z * rot_y_sin},
-            
-        //     Position{x: x * rot_y_cos + z * rot_y_sin, y: y_bot, z: self.position.z + -x * rot_y_sin + z * rot_y_cos},
-        //     Position{x: x * -rot_y_sin + z * rot_y_cos, y: y_bot, z: self.position.z + -x * rot_y_cos + z * -rot_y_sin},
-        //     Position{x: x * -rot_y_cos + z * -rot_y_sin, y: y_bot, z: self.position.z + -x * -rot_y_sin + z * -rot_y_cos},
-        //     Position{x: x * rot_y_sin + z * -rot_y_cos, y: y_bot, z: self.position.z + -x * -rot_y_cos + z * rot_y_sin},
-        // ];
 
         vec![
             Face{corners: [corners[0], corners[1], corners[2], corners[3]], color: Color::White},
@@ -194,34 +174,20 @@ impl Screen {
     fn render_cubie(&mut self, cubie: &Cubie) {
         let mut faces = cubie.faces();
         faces.sort_by(|a, b| a.avg_z().partial_cmp(&b.avg_z()).unwrap());
-
-        // for face in faces.iter() {
-            // println!("avg = {}", face.avg_z());
-        // }
-        // println!("--------------");
         
         for face in faces.into_iter().rev() {
-            // self.print_screen();
-            // std::thread::sleep(Duration::from_millis(1000));
-
-            // println!("avg = {}", face.avg_z());
             let corners = face.corners; 
 
-            // fill faces
             let mut projected: [Point2D; 4] = [Point2D {x: 0, y: 0}; 4];
             for (i, corner) in corners.iter().enumerate() {
                 let multiplier = self.zp / corner.z;
                 let xp = corner.x * multiplier;
                 let yp = corner.y * multiplier;
 
-                // println!("{} : {}", xp, yp);
                 let x_proj = (xp * self.something) as isize + 30;
                 let y_proj = (yp * self.something) as isize + 22;
 
-                // println!("{} : {}", x_proj, y_proj);
-
                 projected[i] = Point2D {x: x_proj, y: y_proj};
-                // self.screen[y_proj as usize][x_proj as usize] = Some(Color::Green);
             }
 
             let tris = [
@@ -256,13 +222,11 @@ impl Screen {
 
     fn print_screen(&self) {
         let char = "██";
-        // let char = "##";
-        let window = 0;
-        for y in (window..(SCREEN_Y-window)).rev() {
-            for x in window..(SCREEN_X-window) {
+
+        for y in (0..(SCREEN_Y)).rev() {
+            for x in 0..(SCREEN_X) {
                 match self.screen[y][x] {
                     Some(color) => print!("{}{char}{}", color.to_ansi(), ANSI_RESET),
-                    // _ => print!("{}{char}{}", Color::White.to_ansi(), ANSI_RESET),
                     _ => print!("  ")
                 };
             }
@@ -281,9 +245,7 @@ fn cube() {
     let mut angle_x = 0.0;
     let mut angle_y = 0.0;
 
-    // let mut somet = 3.0;
     loop {
-        // Screen::clear_screen();
         let cubie = Cubie::new(position, angle_y, angle_x);
         
         let mut screen = Screen::new(ZP, SOMETHING);
@@ -295,21 +257,9 @@ fn cube() {
 
         angle_y += 0.1;
         angle_x += 0.1;
-        // somet += 0.1;
     }
 }
 
 fn main() {
-    // test();
-    
     cube();
-}
-
-fn test() {
-    println!("siema siema\nsienamsi\nasdas\n\nasdasd\nasdas");
-    
-    thread::sleep(Duration::from_millis(500));
-    Screen::move_cursor_up();
-    thread::sleep(Duration::from_millis(500));
-    println!("halo\nhalo\nhalo\n\neeee");
 }
