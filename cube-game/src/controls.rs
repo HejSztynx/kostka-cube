7c4da5e689e3b78fdfa211d31f7084ea956305c7
HejSztynx <shaggyic1@onet.eu>
2025-08-22 17:32:08 +0200

    rest of the moves

--- cube-game/src/controls.rs
+++ cube-game/src/controls.rs
@@ -3,15 +3,45 @@ use std::collections::HashMap;
 use cube_core::cube::core::grid::{GridSide, MoveDirection};
 use winit::keyboard::KeyCode;
 
+// rotation key mapping
+
 pub const ROTATE_X_CODE: KeyCode = KeyCode::KeyS;
 pub const ROTATE_X_PRIM_CODE: KeyCode = KeyCode::KeyW;
+
 pub const ROTATE_Y_CODE: KeyCode = KeyCode::KeyA;
 pub const ROTATE_Y_PRIM_CODE: KeyCode = KeyCode::KeyD;
+
 pub const ROTATE_Z_CODE: KeyCode = KeyCode::KeyQ;
 pub const ROTATE_Z_PRIM_CODE: KeyCode = KeyCode::KeyE;
 
+// move key mapping
+
 pub const MOVE_R_CODE: KeyCode = KeyCode::KeyR;
 pub const MOVE_R_PRIM_CODE: KeyCode = KeyCode::KeyT;
+
+pub const MOVE_L_CODE: KeyCode = KeyCode::KeyY;
+pub const MOVE_L_PRIM_CODE: KeyCode = KeyCode::KeyU;
+
+pub const MOVE_U_CODE: KeyCode = KeyCode::KeyI;
+pub const MOVE_U_PRIM_CODE: KeyCode = KeyCode::KeyO;
+
+pub const MOVE_D_CODE: KeyCode = KeyCode::KeyF;
+pub const MOVE_D_PRIM_CODE: KeyCode = KeyCode::KeyG;
+
+pub const MOVE_F_CODE: KeyCode = KeyCode::KeyH;
+pub const MOVE_F_PRIM_CODE: KeyCode = KeyCode::KeyJ;
+
+pub const MOVE_B_CODE: KeyCode = KeyCode::KeyK;
+pub const MOVE_B_PRIM_CODE: KeyCode = KeyCode::KeyL;
+
+pub const MOVE_M_CODE: KeyCode = KeyCode::KeyX;
+pub const MOVE_M_PRIM_CODE: KeyCode = KeyCode::KeyC;
+
+pub const MOVE_E_CODE: KeyCode = KeyCode::KeyV;
+pub const MOVE_E_PRIM_CODE: KeyCode = KeyCode::KeyB;
+
+pub const MOVE_S_CODE: KeyCode = KeyCode::KeyN;
+pub const MOVE_S_PRIM_CODE: KeyCode = KeyCode::KeyM;
 
 pub fn move_bindings() -> HashMap<KeyCode, (GridSide, MoveDirection)> {
     use GridSide::*;
@@ -21,19 +51,28 @@ pub fn move_bindings() -> HashMap<KeyCode, (GridSide, MoveDirection)> {
         (MOVE_R_CODE, (Right, Clockwise)),
         (MOVE_R_PRIM_CODE, (Right, CounterClockwise)),
 
-        // (KeyCode::KeyL, (Left, Clockwise)),
-        // (KeyCode::KeyK, (Left, CounterClockwise)),
+        (MOVE_L_CODE, (Left, Clockwise)),
+        (MOVE_L_PRIM_CODE, (Left, CounterClockwise)),
 
-        // (KeyCode::KeyU, (Top, Clockwise)),
-        // (KeyCode::KeyJ, (Top, CounterClockwise)),
+        (MOVE_U_CODE, (Top, Clockwise)),
+        (MOVE_U_PRIM_CODE, (Top, CounterClockwise)),
 
-        // (KeyCode::KeyD, (Bottom, Clockwise)),
-        // (KeyCode::KeyF, (Bottom, CounterClockwise)),
+        (MOVE_D_CODE, (Bottom, Clockwise)),
+        (MOVE_D_PRIM_CODE, (Bottom, CounterClockwise)),
 
-        // (KeyCode::KeyF, (Front, Clockwise)),
-        // (KeyCode::KeyG, (Front, CounterClockwise)),
+        (MOVE_F_CODE, (Front, Clockwise)),
+        (MOVE_F_PRIM_CODE, (Front, CounterClockwise)),
 
-        // (KeyCode::KeyB, (Back, Clockwise)),
-        // (KeyCode::KeyN, (Back, CounterClockwise)),
+        (MOVE_B_CODE, (Back, Clockwise)),
+        (MOVE_B_PRIM_CODE, (Back, CounterClockwise)),
+
+        (MOVE_M_CODE, (MiddleX, Clockwise)),
+        (MOVE_M_PRIM_CODE, (MiddleX, CounterClockwise)),
+
+        (MOVE_E_CODE, (MiddleY, Clockwise)),
+        (MOVE_E_PRIM_CODE, (MiddleY, CounterClockwise)),
+        
+        (MOVE_S_CODE, (MiddleZ, Clockwise)),
+        (MOVE_S_PRIM_CODE, (MiddleZ, CounterClockwise)),
     ])
 }
